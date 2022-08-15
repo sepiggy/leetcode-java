98d837000080e69331255875d64f7d4ec0109345
sepiggy <jms1209@qq.com>
2022-08-15 16:30:09 +0800

    p1,p283,p485,p645

---
+++ src/main/java/cn/sepiggy/array/p645/Solution.java
@@ -0,0 +1,34 @@
+package cn.sepiggy.array.p645;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.cn/problems/set-mismatch/
+ */
+class Solution {
+
+	public static int[] findErrorNums(int[] nums) {
+
+		int slow = 0, fast = 1;
+
+		while (fast < nums.length) {
+			if (nums[slow] + 1 == nums[fast]) {
+				slow++;
+				fast++;
+			} else {
+				//
+				break;
+			}
+
+		}
+
+		return new int[]{nums[fast], nums[fast] - 1};
+	}
+
+	public static void main(String[] args) {
+		final int[] errorNums = findErrorNums(new int[]{1, 2, 2, 4});
+		// final int[] errorNums = findErrorNums(new int[]{1, 2, 2});
+		// final int[] errorNums = findErrorNums(new int[]{2, 2});
+		System.out.println("Arrays.toString(errorNums) = " + Arrays.toString(errorNums));
+	}
+}
