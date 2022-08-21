0410984af6dd622b3eb9a5c19fc8c4114e1668f8
sepiggy <jms1209@qq.com>
2022-08-22 00:10:11 +0800

    p94,p98,p100,p144,p145,p501,p572

---
+++ src/main/java/cn/sepiggy/tree/p100/Solution.java
@@ -0,0 +1,34 @@
+package cn.sepiggy.tree.p100;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == q) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val &&
+                isSameTree(p.left, q.left) &&
+                isSameTree(p.right, q.right);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
