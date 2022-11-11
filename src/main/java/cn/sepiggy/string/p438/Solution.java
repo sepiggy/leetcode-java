79abf41a2eb5df8e7f873e17d3f3edca47f3e0a5
sepiggy <jms1209@qq.com>
2022-11-11 20:28:18 +0800

    p438

--- src/main/java/cn/sepiggy/string/p438/Solution.java
+++ src/main/java/cn/sepiggy/string/p438/Solution.java
@@ -1,29 +1,29 @@
-package cn.sepiggy.string.p438;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
-
-        final List<Integer> res = new ArrayList<>();
-
-        final int length = s == null ? 0 : s.length();
-
-        int left = 0, right = 0;
-
-        while (right < length) {
-            final char c = s.charAt(right++);
-
-            while (true) {
-                final char[] chars = s.substring(left, right).toCharArray();
-                Arrays.sort(s.substring(left, right).toCharArray())
-
-            }
-
-
-        }
-
-    }
-}
+//package cn.sepiggy.string.p438;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//class Solution {
+//    public List<Integer> findAnagrams(String s, String p) {
+//
+//        final List<Integer> res = new ArrayList<>();
+//
+//        final int length = s == null ? 0 : s.length();
+//
+//        int left = 0, right = 0;
+//
+//        while (right < length) {
+//            final char c = s.charAt(right++);
+//
+//            while (true) {
+//                final char[] chars = s.substring(left, right).toCharArray();
+//                Arrays.sort(s.substring(left, right).toCharArray())
+//
+//            }
+//
+//
+//        }
+//
+//    }
+//}
