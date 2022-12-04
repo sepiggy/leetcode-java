36795616a08f165af6b553d612a11e4808f34563
sepiggy <jms1209@qq.com>
2022-12-04 19:33:54 +0800

    p50,p493

---
+++ src/main/java/cn/sepiggy/leetcode/recursion/p493/Solution.java
@@ -0,0 +1,7 @@
+//package cn.sepiggy.leetcode.recursion.p493;
+//
+//class Solution {
+//    public int reversePairs(int[] nums) {
+//
+//    }
+//}
--- src/main/java/cn/sepiggy/leetcode/recursion/p50/Solution.java
+++ src/main/java/cn/sepiggy/leetcode/recursion/p50/Solution.java
@@ -1,37 +1,29 @@
 package cn.sepiggy.leetcode.recursion.p50;
 
+/**
+ * 拆分问题 + 组合子问题
+ */
 class Solution {
 
     public double myPow(double x, int n) {
-        if (n == 0) {
-            return 1;
-        }
 
-        if (n == Integer.MIN_VALUE) {
-            // 把 n 是 INT_MIN 的情况单独拿出来处理
-            // 避免 -n 整型溢出
-            return myPow(1 / x, -(n + 1)) / x;
-        }
-
+        long N = n; // 将n转换为长整型，避免溢出
         if (n < 0) {
-            return myPow(1 / x, -n);
+            return myPow(1 / x, -N);
         }
-
-        if (n % 2 == 1) {
-            // n 是奇数
-            return (x * myPow(x, n - 1));
-        } else {
-            // n 是偶数
-            double sub = myPow(x, n / 2);
-            return (sub * sub);
-        }
+        return myPow(x, N);
     }
 
-    public static void main(String[] args) {
-        final Solution solution = new Solution();
-        final double x = 2.00000;
-        final int n = -2;
-        final double result = solution.myPow(x, n);
-        System.out.println("result = " + result);
+    private double myPow(double x, long n) {
+
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
+            return x;
+        } else if (n % 2 == 1) { // n是奇数
+            return x * myPow(x, n - 1);
+        } else { // n是偶数
+            return myPow(x * x, n / 2);
+        }
     }
 }
