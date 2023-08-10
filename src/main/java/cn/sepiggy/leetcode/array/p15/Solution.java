853932b63f3a5b5cae78655b460d1bc9ff750b25
sepiggy <jms1209@qq.com>
2023-08-10 17:05:37 +0800

    p15

---
+++ src/main/java/cn/sepiggy/leetcode/array/p15/Solution.java
@@ -0,0 +1,19 @@
+package cn.sepiggy.leetcode.array.p15;
+
+import java.util.List;
+
+//TODO
+class Solution {
+	public List<List<Integer>> threeSum(int[] nums) {
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				// int p1 =
+
+			}
+
+		}
+
+		return null;
+	}
+}
