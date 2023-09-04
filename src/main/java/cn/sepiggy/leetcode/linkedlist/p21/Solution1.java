6fcb1c7920338026b9fc7c5daebea77f8b25c640
sepiggy <jms1209@qq.com>
2023-09-04 13:07:45 +0800

    p21

--- src/main/java/cn/sepiggy/leetcode/linkedlist/p21/Solution1.java
+++ src/main/java/cn/sepiggy/leetcode/linkedlist/p21/Solution1.java
@@ -1,18 +1,12 @@
 package cn.sepiggy.leetcode.linkedlist.p21;
 
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * https://leetcode.cn/problems/merge-two-sorted-lists/
  */
-class Solution {
+class Solution1 {
 
 	static class ListNode {
+
 		int val;
 		ListNode next;
 
@@ -31,19 +25,20 @@ class Solution {
 
 	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-		ListNode dummyNode = new ListNode();
-		ListNode p = dummyNode;
-		ListNode p1 = list1;
-		ListNode p2 = list2;
+		ListNode dummy = new ListNode(-1), p = dummy;
+		ListNode p1 = list1, p2 = list2;
 
+		// p1和p2两个指针都没到头
 		while (p1 != null && p2 != null) {
+
 			if (p1.val <= p2.val) {
-				p.next = new ListNode(p1.val);
+				p.next = p1;
 				p1 = p1.next;
 			} else {
-				p.next = new ListNode(p2.val);
+				p.next = p2;
 				p2 = p2.next;
 			}
+
 			p = p.next;
 		}
 
@@ -55,6 +50,7 @@ class Solution {
 			p.next = p2;
 		}
 
-		return dummyNode.next;
+		return dummy.next;
 	}
+
 }
